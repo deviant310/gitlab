cbc84438c1ee1768f116ad0c9a853867e0130837
Anton Lebedev <dunloop8@gmail.com>
2021-10-20 16:28:07 +0300

    backup hook fixed

--- hooks/backup.sh
+++ hooks/backup.sh
@@ -4,7 +4,7 @@ gitlab-backup create BACKUP=latest
 
 ssh -f -o StrictHostKeyChecking=no -o LogLevel=ERROR "$APP_USER@$APP_HOST" "
   export PATH=$APP_PATH;
-  git -C $APP_ROOT add backups
+  git -C $APP_ROOT add data/backups
   git -C $APP_ROOT commit -m \"Automatic backup\"
   git -C $APP_ROOT push
 "
