90ab3a5b7ad98bd222d665fb0959ab23a1132b51
Anton Lebedev <dunloop8@gmail.com>
2021-10-20 16:21:00 +0300

    running with docker-compose

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM gitlab/gitlab-ee:13.5.1-ee.0
-
-COPY ./config/* /etc/gitlab/
-COPY ./backups/* /var/opt/gitlab/backups/
-COPY ./hooks/* /opt/gitlab/embedded/service/gitlab-rails/file_hooks/
--- hooks/backup.sh
+++ hooks/backup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-CONTAINER_ID=$(cut -c9- < /proc/1/cpuset)
-
 gitlab-backup create BACKUP=latest
 
 ssh -f -o StrictHostKeyChecking=no -o LogLevel=ERROR "$APP_USER@$APP_HOST" "
   export PATH=$APP_PATH;
-  docker cp $CONTAINER_ID:/var/opt/gitlab/backups/latest_gitlab_backup.tar $APP_ROOT/backups/
-  docker cp $CONTAINER_ID:/etc/gitlab/gitlab.rb $APP_ROOT/config/
-  docker cp $CONTAINER_ID:/etc/gitlab/gitlab-secrets.json $APP_ROOT/config/
   git -C $APP_ROOT add backups
   git -C $APP_ROOT commit -m \"Automatic backup\"
   git -C $APP_ROOT push
