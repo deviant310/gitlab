59c595c65f5a7174ec59f85327d48f13071462a0
Anton Lebedev <dunloop8@gmail.com>
2021-10-20 13:53:55 +0300

    backup hook git command updated

--- hooks/backup.sh
+++ hooks/backup.sh
@@ -9,7 +9,7 @@ ssh -f -o StrictHostKeyChecking=no -o LogLevel=ERROR "$APP_USER@$APP_HOST" "
   docker cp $CONTAINER_ID:/var/opt/gitlab/backups/latest_gitlab_backup.tar $APP_ROOT/backups/
   docker cp $CONTAINER_ID:/etc/gitlab/gitlab.rb $APP_ROOT/config/
   docker cp $CONTAINER_ID:/etc/gitlab/gitlab-secrets.json $APP_ROOT/config/
-  git add .
-  git commit -m \"Automatic backup\"
-  git push
+  git -C $APP_ROOT add backups
+  git -C $APP_ROOT commit -m \"Automatic backup\"
+  git -C $APP_ROOT push
 "
